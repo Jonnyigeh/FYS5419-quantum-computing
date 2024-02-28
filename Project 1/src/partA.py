1a91f962298efb04fa61e01f0aaa17a5ef65ac19
Jonny Aarstad Igeh <jonny.igeh@gmail.com>
2024-02-28 21:13:32 +0100

    Finalized task a and b, also minor update in .gitignore file

---
+++ Project 1/src/partA.py
@@ -0,0 +1,157 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def init_basis() -> np.array:
+    """
+    Set the basis for the qubits, using the two dimensional
+    computational basis |0> = [1,0] and |1> = [0,1].
+    """
+    q0 = np.array([1, 0])       # |0>
+    q1 = np.array([0, 1])       # |1>    
+    
+    return q0, q1
+
+def hadamard_gate(qubit: np.array) -> np.array:
+    """
+    Apply the Hadamard gate to the qubit.
+    """
+    H = 1/np.sqrt(2) * np.array([[1, 1], [1, -1]])
+    qubit = H @ qubit
+    
+    return qubit
+
+def phase_gate(qubit: np.array) -> np.array:
+    """
+    Apply the phase gate to the qubit.
+    """
+    S = np.array([[1, 0], [0, 1j]])
+    qubit = S @ qubit
+    
+    return qubit
+
+def cnot_gate(control_qubit: np.array, target_qubit: np.array) -> (np.array, np.array):
+    """
+    Apply the 2 qubit CNOT gate to the target and control qubit
+    """
+    if np.all(control_qubit == np.array([0,1])):        # Checks if control qubit is infact |1>, then applies the pauli x gate
+        target_qubit = pauli_x(target_qubit)            # If not, returns the original qubits
+    
+
+    return control_qubit, target_qubit
+
+def pauli_x(qubit: np.array) -> np.array:
+    """
+    Apply the Pauli-X gate to the qubit.
+    """
+    X = np.array([[0, 1], [1, 0]])
+    qubit = X @ qubit
+    
+    return qubit
+
+def pauli_y(qubit: np.array) -> np.array:
+    """
+    Apply the Pauli-Y gate to the qubit.
+    """
+    Y = np.array([[0, -1j], [1j, 0]]) 
+    qubit = Y @ qubit
+    
+    return qubit
+
+def pauli_z(qubit: np.array) -> np.array:
+    """
+    Apply the Pauli-Z gate to the qubit.
+    """
+    Z = np.array([[1, 0], [0, -1]])    
+    qubit = Z @ qubit
+    
+    return qubit
+
+def bell_states() -> (np.array, np.array, np.array, np.array):
+    """
+    Sets up the Bell states, using the 2D computational basis.
+    """
+    q0, q1 = init_basis()
+    phi_plus = 1 / np.sqrt(2) * (np.kron(q0, q0) + np.kron(q1, q1))
+    phi_minus = 1 / np.sqrt(2) * (np.kron(q0, q0) - np.kron(q1, q1))
+
+    psi_plus = 1 / np.sqrt(2) * (np.kron(q0, q1) + np.kron(q1, q0))
+    psi_minus = 1 / np.sqrt(2) * (np.kron(q0, q1) - np.kron(q1, q0))
+
+    return phi_plus, phi_minus, psi_plus, psi_minus
+
+def hadamard_gate_bell(bellstate: np.array) -> np.array:
+    """
+    Apply the Hadamard gate to the first qubit in a Bell state
+    """
+    H = 1/np.sqrt(2) * np.array([[1, 1], [1, -1]])
+    new_state = np.kron(H, np.eye(2)) @ bellstate
+    
+    return new_state
+
+def cnot_gate_bell(bellstate: np.array) -> np.array:
+    """
+    Apply the CNOT gate to a Bell state using the convention 
+    that qubit 1 is the control qubit and qubit 2 is the target qubit.
+    """
+    X = np.array([[0, 1], [1, 0]])
+    if (bellstate[2] or bellstate[3]) == 1:        # Checks if control qubit is infact |1>, then applies the pauli x gate
+        new_state = np.kron(np.eye(2), X) @ bellstate
+
+        return new_state
+    return bellstate
+
+def bell_measurements(bellstate: np.array, n_measurements:  int) -> np.array:
+    """
+    Perform measurements on a Bell state, which have the following
+    First index = |00>
+    Second index = |01>
+    Third index = |10>
+    Fourth index = |11>
+    """
+    measurements = np.zeros(n_measurements)
+    for i in range(n_measurements):
+        prob = np.abs(bellstate)**2
+        measurements[i] = np.random.choice([0, 1, 2, 3], p=prob)    # Picks a random index according to the probability from the Bell state
+
+    return measurements
+
+def plot_bell_measurements(measurements: np.array) -> None:
+    """
+    Plot the measurements of a Bell state.
+    """
+    fig, ax = plt.subplots()
+    ax.hist(measurements, bins=[0, 1, 2, 3, 4], align="left")
+    ax.set_xticks([0, 1, 2, 3])
+    ax.set_xticklabels([r'$|00\rangle$', r'$|01\rangle$', r'$|10\rangle$', r'$|11\rangle$'])
+    ax.set_xlabel('Basis')
+    ax.set_ylabel('Counts')
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    # Setting up the one qubit basis
+    q0, q1 = init_basis()
+    # Applying the Pauli gates to the qubits
+    q0x = pauli_x(q0)
+    q0y = pauli_y(q0)
+    q0z = pauli_z(q0)
+    q1x = pauli_x(q1)
+    q1y = pauli_y(q1)
+    q1z = pauli_z(q1)
+    # Applying the Hadamard and Phase gates to the qubits
+    q0h = hadamard_gate(q0)
+    q1h = hadamard_gate(q1)
+    q0s = phase_gate(q0)
+    q1s = phase_gate(q1)
+    # Defining the Bell states
+    phi_plus, phi_minus, psi_plus, psi_minus = bell_states()
+    # Applying the Hadamard and CNOT gates to the Phi+ Bell state
+    H_phi_plus = hadamard_gate_bell(phi_plus)
+    cnot_phiplus = cnot_gate_bell(phi_plus)
+    # Performing measurements on the Phi+ Bell state
+    measurements = bell_measurements(phi_plus, 1000)
+    # Plotting the measurements
+    plot_bell_measurements(measurements)    
+    
+
---
+++ Project 1/src/partB.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from partA import init_basis
+
+# Constants
+E1 = 0
+E2 = 4
+V11 = 3
+V12 = 0.2
+V21 = 0.2
+V22 = - V11
+
+def H():
+    """
+    Returns the Hamiltonian matrix for the system.
+    """
+    H0 = np.array([[E1, 0], [0, E2]])
+    H1 = np.array([[V11, V12], [V21, V22]])
+
+    return H0 + H1
+
+def H_pauli(lmbda: float):
+    """
+    Returns the Hamiltonian matrix, written using the Pauli matrices
+    """
+    # Pauli matrices
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1j], [1j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+
+    Eps = (E1 + E2) / 2
+    Omega = (E1 - E2) / 2
+    c = (V11 + V22) / 2
+    omega_z = (V11 - V22) / 2   
+    omega_x = V21
+
+    H0 = Eps * np.eye(2) + Omega * Z
+    H1 = c * np.eye(2) + omega_z * Z + omega_x * X
+
+    return H0 + lmbda * H1
+
+if __name__ == "__main__":
+    # Initialize the basis
+    q0, q1 = init_basis()
+    lmbda = np.linspace(0, 1, 10)
+    eigvals = np.zeros((len(lmbda), 2))
+    eigvecs = np.zeros((len(lmbda), 2, 2))
+    for i, l in enumerate(lmbda):
+        hamiltonian = H_pauli(l)
+        eigvals[i], eigvecs[i] = np.linalg.eigh(hamiltonian)
+
+    sns.set_theme()
+    fig, ax = plt.subplots()
+    ax.plot(lmbda, eigvals[:, 0], label="E1", color="blue", linestyle="--")
+    ax.plot(lmbda, eigvals[:, 1], label="E2", color="red", linestyle="--")
+    ax.scatter(lmbda, eigvals[:, 0], color="blue", marker="x")
+    ax.scatter(lmbda, eigvals[:, 1], color="red", marker="x")
+    ax.annotate(r"$\lambda = \frac{2}{3}$", xy=(2/3, 2.3), xytext=(2/3, 3), arrowprops=dict(color="black", arrowstyle="->"), ha="center")
+    ax.set_xlabel(r"$\lambda$")
+    ax.set_ylabel("Energy")
+    ax.set_title("Energy levels as a function of $\lambda$," + "\n" + "highlighting the avoided crossing")
+    ax.legend()
+    plt.show()    
