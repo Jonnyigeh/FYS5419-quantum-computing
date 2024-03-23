d000543961b4066ed360ddb86864eeee91084777
Jonny Aarstad Igeh <jonny.igeh@gmail.com>
2024-03-23 14:53:23 +0100

    Added files for plotting two of the subtasks E and G

---
+++ Project 1/src/partEplot.py
@@ -0,0 +1,25 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == "__main__":
+    # Load the eigenvalues, both exact and VQE
+    with open("../doc/eigenvalues.json", "r") as file:
+        eigenvalues = json.load(file)
+    with open("../doc/estimated_eigenvalues.json", "r") as file:
+        estimated_eigenvalues = json.load(file)
+    
+    # Plot the estimated eigenvalues against the exact eigenvalues
+    sns.set_theme()
+    fig, ax = plt.subplots()
+    x_axis = eigenvalues.keys()
+    ax.scatter(x_axis, eigenvalues.values(), label="Exact eigenvalues", color="tomato", marker="x")
+    ax.plot(x_axis, estimated_eigenvalues.values(), label="VQE estimated eigenvalues", color="cornflowerblue", linestyle="--")
+    ax.set_xlabel(r"$\lambda$")
+    ax.set_ylabel(r"Energy eigenvalue")
+    ax.set_title(r"Exact eigenvalues vs. VQE estimated eigenvalues")
+    ax.legend()
+    fig.savefig("../doc/figs/energy_eigenvalues_twoqubit_VQE.pdf")
+    # plt.show()
+    
